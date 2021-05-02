6103d5fa4148357fb7b79150baf8a796c93e67ce
Colten Krauter <coltenkrauter@gmail.com>
2021-05-02 12:29:31 -0700

    Update index.tsx

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -19,7 +19,7 @@ const Component = () => {
         <div className='content'>
 
       <iframe width="560" height="315" src="https://www.youtube.com/embed/j9mJkvJEE1k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <br><br>
+      <br /><br />
 Join a memorial service for Valerie Othus. 
 
 Please share memories here: <a href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa2JUeUZkWkRzMmIwWmdlRGJjWVF1RVB2Smp2d3xBQ3Jtc0tsckNHc0tGSExSN2VDYWlLVjVUUTB2bWJNRU54S1RrelZnWVRiT0MwSU9sOGd0VUF6SlVVbjZmUURsZUJDMEhna09RWVR0Z3ZnTTNvYUcxby1xZVdkbVNwQUgyb1lXQzhqLWt5TmR1OHliaWNrTmRDSQ&q=https%3A%2F%2Fobituaries.neptunesociety.com%2Fobituaries%2Ftacoma-wa%2Fvalerie-othus-10138907" target="_blank">https://obituaries.neptunesociety.com...</a>

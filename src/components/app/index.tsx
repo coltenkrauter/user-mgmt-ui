28cbd8436a6534ee21756b41da0c204c61a58b29
Colten Krauter <coltenkrauter@gmail.com>
2021-05-02 12:52:50 -0700

    Update index.tsx

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -18,7 +18,7 @@ const Component = () => {
         <Header />
         <div className='content'>
 
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/j9mJkvJEE1k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/j9mJkvJEE1k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <br /><br />
 Join a memorial service for Valerie Othus. 
 
